bcb25c46770eecddb7e4e2625f7cb03e72dff7b6
Mark Lightfoot <mark.lightfoot@cloudsolutions.co.uk>
2021-11-01 16:23:52 +0000

    update

--- infra/tf/terraform/bucket/variables.tf
+++ infra/tf/terraform/bucket/variables.tf
@@ -12,10 +12,11 @@ variable "iam" {
 
 variable "buckets" {
   type = list(object({
-    name          = string
-    location      = string
-    force_destroy = optional(bool)
-    storage_class = optional(string)
-    versioning    = optional(bool)
+    name                        = string
+    location                    = string
+    force_destroy               = optional(bool)
+    storage_class               = optional(string)
+    versioning                  = optional(bool)
+    uniform_bucket_level_access = optional(bool)
   }))
 }
